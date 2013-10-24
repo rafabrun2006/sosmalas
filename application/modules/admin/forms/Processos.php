44058cd3e341589a6a78533176887817e4331c04
Rafael Bruno <rafabrun2006@gmail.com>
2013-10-24 03:19:38 -0200

    Voltando homologa

--- application/modules/admin/forms/Processos.php
+++ application/modules/admin/forms/Processos.php
@@ -18,42 +18,46 @@ class Admin_Form_Processos extends Zend_Form {
 
         $id_processo = new Zend_Form_Element_Hidden('id_processo');
         $this->addElement($id_processo);
-        
+
         $cod_processo = new Zend_Form_Element_Text('cod_processo');
         $cod_processo->setLabel('Processo: ');
         $this->addElement($cod_processo);
-        
+
         $id_empresa = new Zend_Form_Element_Select('id_empresa');
         $id_empresa->setLabel('Parceiro: ');
         $this->addElement($id_empresa);
-        
+
         $nome_cliente = new Zend_Form_Element_Text('nome_cliente');
         $nome_cliente->setLabel('Cliente');
         $this->addElement($nome_cliente);
-        
+
         $quantidade = new Zend_Form_Element_Text('quantidade');
         $quantidade->setLabel('Quantidade');
         $this->addElement($quantidade);
-        
+
         $descricao_produto = new Zend_Form_Element_Text('descricao_produto');
         $descricao_produto->setLabel('Produto/Marca/Modelo/Cor: ');
         $this->addElement($descricao_produto);
-        
+
         $conserto = new Zend_Form_Element_Text('conserto');
         $conserto->setLabel('Conserto: ');
         $this->addElement($conserto);
-        
-        $dt_coleta = new Zend_Form_Element_Text('dt_coleta', array('class'=>'date'));
+
+        $dt_coleta = new Zend_Form_Element_Text('dt_coleta', array('class' => 'date'));
         $dt_coleta->setLabel('Data Coleta: ');
         $this->addElement($dt_coleta);
-        
-        $dt_entrega = new Zend_Form_Element_Text('dt_entrega', array('class'=>'date'));
+
+        $dt_entrega = new Zend_Form_Element_Text('dt_entrega', array('class' => 'date'));
         $dt_entrega->setLabel('Data Entrega: ');
         $this->addElement($dt_entrega);
-        
+
         $status_id = new Zend_Form_Element_Select('status_id');
         $status_id->setLabel('Status');
         $this->addElement($status_id);
+
+        $pessoa_cadastro_id = new Zend_Form_Element_Hidden('pessoa_cadastro_id');
+        $pessoa_cadastro_id->setRequired(true);
+        $this->addElement($pessoa_cadastro_id);
 
         $this->populaComboEmpresa();
         $this->populaComboStatus();

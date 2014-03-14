9bb08b0c2bd5bc26e9bc51f46685a0c88fb6c85a
Rafael Bruno <rafabrun2006@gmail.com>
2014-03-14 02:25:37 -0300

    Alteracoes na funcionalidade de processos

---
+++ application/modules/admin/views/scripts/processos/processos.js
@@ -0,0 +1,74 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+app = angular.module('App', ['ui.mask', 'ui.bootstrap.pagination']);
+
+app.controller('ProcessosController', function($scope, $http) {
+    $scope.collection = [];
+    $scope.dtEntrega = null;
+    $scope.dtColeta = null;
+
+    loading();
+    $http.post('/admin/processos/find-vw-processos')
+            .success(function(result) {
+                $scope.collection = result;
+                loaded();
+            })
+            .error(function() {
+                console.log('Error');
+            });
+
+    $scope.filterDateInverse = function() {
+        if ($scope.dtColeta !== null) {
+            var split = $scope.dtColeta.split('-');
+            return (split[2] ? split[2] + '-' : '') + (split[1] ? split[1] + '-' : '') + (split[0] ? split[0] : '');
+        }
+    };
+
+    function dateToEua(date) {
+        var split = date.replace('-', '').split('');
+        var data = {date: split[0] + split[1], month: split[2] + split[3], year: split[4] + split[5] + split[6] + split[7]};
+        return (data.year ? data.year + '-' : '') + (data.month ? data.month + '-' : '') + (data.date ? data.date : '');
+    }
+
+    $scope.filterDataColeta = function(data) {
+
+        if ($scope.dtColeta != null) {
+            var dtColeta = dateToEua($scope.dtColeta);
+            if (data >= dtColeta) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if ($scope.dtColeta == null) {
+            return true;
+        }
+    };
+
+    $scope.filterDataEntrega = function(data) {
+        if ($scope.dtEntrega != null) {
+            var dtEntrega = dateToEua($scope.dtEntrega);
+            if (data <= dtEntrega) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if ($scope.dtEntrega == null) {
+            return true;
+        }
+    };
+    
+    $scope.dateToBrView = function(dateString){
+        var data = dateString.split('-');
+        return data[2] + '-' + data[1] + '-' + data[0];
+    };
+
+    $scope.resetForm = function() {
+        $scope.dtColeta = null;
+    };
+
+});
+
+
